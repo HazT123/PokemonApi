7cc3244640648b7221c655462cfa7b88682e612e
Harry Tomlinson <harry.t.123@hotmail.com>
2024-04-20 13:33:17 +0100

    Initial Commit

---
+++ PokemonApi/PokemonApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApiApp.swift
+//  PokemonApi
+//
+//  Created by Harry Tomlinson on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
